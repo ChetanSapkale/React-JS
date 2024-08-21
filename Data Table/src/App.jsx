793ff7f3871ba02ca3bc6516b3dfff12a8d8da9a
ChetanSapkale <sapkalechetan018@gmail.com>
2024-08-21 17:12:18 +0530

    updated

---
+++ Data Table/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import ProductList from './Components/ProductList'
+import "./index.css"
+import All from './Components/All'
+
+function App() {
+
+
+  return (
+    <>
+      <All />
+    </>
+  )
+}
+
+export default App
